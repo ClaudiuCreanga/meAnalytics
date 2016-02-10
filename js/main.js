e192b9ae68ae42cd27c3db3783afb4f70e9a04c6
Claudiu Creanga <claudiu@limesharp.net>
2016-02-10 00:24:36 +0000

    fix the sidebar and add elements

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 var menu = document.querySelector('.menu');
-var menu_container = document.querySelector('.menu-container');
-var websites = document.querySelector('.websites');
+	menu_container = document.querySelector('.menu-container');
+	websites = document.querySelector('.websites');
+	add_item = document.querySelector('.add-item');
+	container_items = document.querySelector('.list-items');
+	
 menu_container.addEventListener("click",function(e){
 	var isOpen = websites.classList.contains('slide-in');
 	var isClosed = websites.classList.contains('slide-out');
@@ -8,3 +11,14 @@ menu_container.addEventListener("click",function(e){
 	websites.className = isOpen || isClosed ? websites.className += ' active' : '';
     websites.className = isOpen ? websites.className.replace('slide-in','slide-out') : websites.className.replace('slide-out','slide-in');
 },false);
+
+add_item.addEventListener("click", function(e){
+	
+	var items = document.querySelectorAll('.item');
+		item = items[items.length - 1];
+		item_number = item.className.match(/\d+$/);
+		new_item = item.cloneNode(true);
+		
+	container_items.appendChild(new_item);
+	new_item.className = new_item.className.replace(/[0-9]/g,+item_number+1);
+},false)
