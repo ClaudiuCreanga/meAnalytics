b9080fb413ce78ebc8e741effd9e278aa9b27378
Claudiu Creanga <claudiu@limesharp.net>
2016-02-10 23:52:36 +0000

    fix add and remove functionality

--- js/main.js
+++ js/main.js
@@ -12,6 +12,12 @@ menu_container.addEventListener("click",function(e){
     websites.className = isOpen ? websites.className.replace('slide-in','slide-out') : websites.className.replace('slide-out','slide-in');
 },false);
 
+var remove_item = function(removable_item){
+	removable_item.addEventListener('click', function(e){
+		removable_item.parentElement.remove();
+	},false);
+}
+
 add_item.addEventListener("click", function(e){
 	
 	var items = document.querySelectorAll('.item');
@@ -19,6 +25,19 @@ add_item.addEventListener("click", function(e){
 		item_number = item.className.match(/\d+$/);
 		new_item = item.cloneNode(true);
 		
-	container_items.appendChild(new_item);
 	new_item.className = new_item.className.replace(/[0-9]/g,+item_number+1);
-},false)
+	var labels_inputs = new_item.querySelector('.switch');
+		labels = labels_inputs.getElementsByTagName('label');
+		inputs = labels_inputs.getElementsByTagName('input');
+		removable_item = new_item.querySelector('.remove-item');
+	remove_item(removable_item);
+	
+	for(var i, i = 0; i < labels.length; i++){
+		inputs[i].id = inputs[i].id.replace(/[0-9]/g, +item_number+1);
+		inputs[i].name = inputs[i].name.replace(/[0-9]/g, +item_number+1);
+		labels[i].htmlFor = inputs[i].id;
+	}
+	container_items.appendChild(new_item);
+	
+},false);
+
