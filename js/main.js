a34fd5e00df2d5812293457485032e12c0763ff5
Claudiu Creanga <claudiu@limesharp.net>
2016-02-12 01:33:30 +0000

    storage in chrome

--- js/main.js
+++ js/main.js
@@ -12,9 +12,39 @@ menu_container.addEventListener("click",function(e){
     websites.className = isOpen ? websites.className.replace('slide-in','slide-out') : websites.className.replace('slide-out','slide-in');
 },false);
 
+
+var number_of_websites = 1;
+var decrease_websites = function(number_of_websites){
+	number_of_websites -= 1;
+	storeWebsites();
+}
+var increase_websites = function(number_of_websites){
+	number_of_websites += 1;
+	storeWebsites();
+}
+function storeWebsites(){
+	chrome.storage.sync.set(settings, function () {
+        console.log('Saved', settings);
+    });
+}
+
+var settings = {};
+var list_inputs = websites.getElementsByTagName('input');
+for(var i, i = 0; i < list_inputs.length; i++){
+	list_inputs[i].addEventListener('keyup', function(){
+		var input_value = this.value;
+		var key = this.parentElement.className;
+		settings["website"] = input_value;
+		settings["key"] = key;
+		console.log(key)
+		increase_websites();
+	},false)
+}
+
 var remove_item = function(removable_item){
 	removable_item.addEventListener('click', function(e){
 		removable_item.parentElement.remove();
+		decrease_websites;
 	},false);
 }
 
@@ -38,6 +68,6 @@ add_item.addEventListener("click", function(e){
 		labels[i].htmlFor = inputs[i].id;
 	}
 	container_items.appendChild(new_item);
-	
+		
 },false);
 
